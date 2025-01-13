110aeea807c00b5a94570af0958d4aa07fe05c1b
Bruno Seiji <88354853+bruno-seiji@users.noreply.github.com>
2025-01-13 15:57:33 -0300

    .

--- script.js
+++ script.js
@@ -1,19 +1,2 @@
-// --------------------Glide.js---------------------------------
-document.addEventListener('DOMContentLoaded', () => {
-  const swiper = new Swiper('.swiper', {
-    loop: true, // Faz os slides girarem em loop
-    pagination: {
-      el: '.swiper-pagination',
-      clickable: true,
-    },
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
-   
-  });
-});
-//--------------------------------------------------------------
-
 
 
