324fc77a83bc6a0480b47134a1960bd9290515b7
Bruno Seiji <88354853+bruno-seiji@users.noreply.github.com>
2025-01-13 17:00:56 -0300

    .

--- script.js
+++ script.js
@@ -1,2 +1,12 @@
+var header = document.querySelector("header")
 
+window.addEventListener('scroll', () => {
+  // Verifica se o scroll vertical passou de 100px
+  if (window.scrollY > 100) {
+    header.classList.add("fixed")
+  }
+  else{
+    header.classList.remove("fixed")
+  }
+});
 
