1d453d7576793525223d7539f77555530d812731
Bruno Seiji <88354853+bruno-seiji@users.noreply.github.com>
2025-02-04 13:18:37 -0300

    .

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import styled from "styled-components";
+import "../src/App.css"
+import Header from "../src/components/header.jsx"
+
+
+
+
+export default function App() {
+
+  return (
+   <div className="App_container">
+      <Header></Header>
+   </div>
+  )
+
+}
+
+
