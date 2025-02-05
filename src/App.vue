4a814403b12bbde05ae0f6c8f0c0cd45579e2de6
Bruno Seiji <88354853+bruno-seiji@users.noreply.github.com>
2025-02-05 14:57:14 -0300

    .

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup>
+  import TheHeader from "../src/components/header.vue"
+</script>
+
+<template>
+  <div class="app_container">
+    <TheHeader></TheHeader>
+  </div>
+
+</template>
+
+<style scoped>
+
+
+</style>
---
+++ src/components/header.vue
@@ -0,0 +1,30 @@
+<template>
+
+    <header>
+        <div class="logo"></div>
+        <div class="menu"></div>
+    </header>
+
+</template>
+
+<script></script>
+
+
+<style>
+
+.TheHeader{
+    margin: auto;
+    width: 1270px;
+    position: relative;
+    display: flex;
+
+    .logo{
+      background-image: url("../assets/logo-SF.png");
+      background-position: center;
+      background-repeat: no-repeat;
+      height: 60px;
+      weight: 80px ;
+      color: black;
+    }
+  }
+</style>
